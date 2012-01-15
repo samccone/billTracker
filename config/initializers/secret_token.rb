409a46aa5d8ea39dd4adf552b2ae81e475ae9909
Sam Saccone <samccone@gmail.com>
2012-01-15 10:58:55 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobTracker::Application.config.secret_token = '70f3a412a579655768f70c76e58b6052330c642c7e326b40b308663b95aecad63a77226bd08e6d2056e298c1222ce3d8c9ef10201ee2240c7ac87d3b2da7fe0e'
