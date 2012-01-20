ca65d5ebd864a8d8147b7d81e4196154d94be866
Sam Saccone <samccone@gmail.com>
2012-01-19 22:08:03 -0500

    added scopes and real counts for selectors

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ActiveRecord::Base
+  scope :complete, where(:delivered => false)
+  scope :payment_pending, where(:delivered => true, :payment_received => false)
+  scope :done, where(:delivered => true, :payment_received => true)
 end
